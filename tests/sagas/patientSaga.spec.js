32be3a4d44969a3ad9b40be47e336d4c56780928
jomadoye <jedidiah.omadoye@andela.com>
2018-09-04 14:30:23 +0100

    Add test for patientSaga

---
+++ tests/sagas/patientSaga.spec.js
@@ -0,0 +1,27 @@
+import patientSagas from '../../app/js/sagas/patientSaga';
+import actionTypes from '../../app/js/actions/actionTypes';
+
+let sagaTester;
+
+const { SagaTester } = global;
+
+describe('patientSaga', () => {
+  beforeEach(() => {
+    sagaTester = new SagaTester({});
+    sagaTester.start(patientSagas);
+  });
+
+  it('should run getPatientRecord() saga', () => {
+    expect(sagaTester.getCalledActions()).toEqual([]);
+    expect(sagaTester.wasCalled(actionTypes.SET_PATIENT.REQUESTED)).toEqual(false);
+    sagaTester.dispatch({ type: actionTypes.SET_PATIENT.REQUESTED });
+    expect(sagaTester.wasCalled(actionTypes.SET_PATIENT.REQUESTED)).toEqual(true);
+  });
+
+  it('should run getPatientNote() saga', () => {
+    expect(sagaTester.getCalledActions()).toEqual([]);
+    expect(sagaTester.wasCalled(actionTypes.SET_PATIENT_NOTE.REQUESTED)).toEqual(false);
+    sagaTester.dispatch({ type: actionTypes.SET_PATIENT_NOTE.REQUESTED });
+    expect(sagaTester.wasCalled(actionTypes.SET_PATIENT_NOTE.REQUESTED)).toEqual(true);
+  });
+});
