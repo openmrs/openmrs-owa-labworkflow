bd8ee6eddd09c933e2d1654a87c52438fb45f8a4
rowland henshaw <rowland.henshaw@gmail.com>
2018-09-17 16:05:15 +0100

    removed eslint disabling

--- app/js/utils/constants.js
+++ app/js/utils/constants.js
@@ -1,7 +1,7 @@
-/* eslint-disable*/
 export const DEFAULT_DATE_FORMAT = 'D-MMM-YYYY';
+
 export const ENCOUNTER_TYPES = {
   specimenCollectionEncounterType: {
     uuid: '10db3139-07c0-4766-b4e5-a41b01363145',
-  }
+  },
 };
