40bd23c258a32ab03e90a4cc907f645a7088a576
jomadoye <omadoyejed@gmail.com>
2018-09-26 17:38:16 +0100

    lint PR

--- app/js/reducers/constantsReducer.js
+++ app/js/reducers/constantsReducer.js
@@ -3,7 +3,7 @@ import {
   FETCH_LAB_RESULTS_ENCOUNTER_TYPE,
   FETCH_LAB_RESULTS_DATE_CONCEPT,
   GET_DATE,
-    FETCH_LAB_RESULTS_DID_NOT_PERFORM_ANSWER,
+  FETCH_LAB_RESULTS_DID_NOT_PERFORM_ANSWER,
   FETCH_LAB_RESULTS_DID_NOT_PERFORM_QUESTION,
   FETCH_LAB_RESULTS_DID_NOT_PERFORM_REASON,
 } from '../actions/actionTypes';
@@ -34,16 +34,16 @@ export default (state = initialState.CONSTANTS, action) => {
         isLoading: true,
       };
     }
-      
-        case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_ANSWER}_SUCCESS`: {
+
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_ANSWER}_SUCCESS`: {
       return {
         ...state,
         isLoading: false,
         labResultsDidNotPerformAnswer: action.payload.data.results[0].value,
       };
     }
-      
-     case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_ANSWER}_FAILURE`: {
+
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_ANSWER}_FAILURE`: {
       return {
         ...state,
         isLoading: false,
@@ -53,22 +53,22 @@ export default (state = initialState.CONSTANTS, action) => {
         },
       };
     }
-      
-     case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_ANSWER}_LOADING`: {
+
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_ANSWER}_LOADING`: {
       return {
         ...state,
         isLoading: true,
       };
     }
-     case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_QUESTION}_SUCCESS`: {
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_QUESTION}_SUCCESS`: {
       return {
         ...state,
         isLoading: false,
         labResultsDidNotPerformQuestion: action.payload.data.results[0].value,
       };
     }
-      
-     case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_QUESTION}_FAILURE`: {
+
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_QUESTION}_FAILURE`: {
       return {
         ...state,
         isLoading: false,
@@ -78,23 +78,23 @@ export default (state = initialState.CONSTANTS, action) => {
         },
       };
     }
-      
-     case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_QUESTION}_LOADING`: {
+
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_QUESTION}_LOADING`: {
       return {
         ...state,
         isLoading: true,
       };
     }
-      
-     case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_REASON}_SUCCESS`: {
+
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_REASON}_SUCCESS`: {
       return {
         ...state,
         isLoading: false,
         labResultsDidNotPerformReason: action.payload.data.results[0].value,
       };
     }
-      
-     case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_REASON}_FAILURE`: {
+
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_REASON}_FAILURE`: {
       return {
         ...state,
         isLoading: false,
@@ -104,8 +104,8 @@ export default (state = initialState.CONSTANTS, action) => {
         },
       };
     }
-      
-     case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_REASON}_LOADING`: {
+
+    case `${FETCH_LAB_RESULTS_DID_NOT_PERFORM_REASON}_LOADING`: {
       return {
         ...state,
         isLoading: true,
