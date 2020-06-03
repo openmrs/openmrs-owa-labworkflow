7163e7f2acc3a1e8e9aec4a5d1afcad9ea0be67f
Brandon Istenes <bistenes@gmail.com>
2020-06-02 17:47:44 -0700

    Autoformat cypress files (#184)

--- cypress/integration/labResultEntryPage.spec.jsx
+++ cypress/integration/labResultEntryPage.spec.jsx
@@ -1,168 +1,144 @@
-import { MOCKS } from '../support/constants';
+import { MOCKS } from "../support/constants";
 
-describe('Lab Result Entry Page', () => {
-  describe('Navigate', () => {
+describe("Lab Result Entry Page", () => {
+  describe("Navigate", () => {
     beforeEach(() => {
       cy.login();
-      cy.server()
-        .fetchOrders()
-        .fetchEncounters();
+      cy.server().fetchOrders().fetchEncounters();
     });
 
     afterEach(() => {
       cy.navigateToHomePage();
     });
-    it('should navigate to the lab result entry page when an ordered order is clicked', () => {
+
+    it("should navigate to the lab result entry page when an ordered order is clicked", () => {
       cy.navigateToLabWorkflow();
 
-      cy.get('#test-status-dropdown')
-      .select('Ordered');
+      cy.get("#test-status-dropdown").select("Ordered");
 
-      cy.get('.rt-tbody')
-      .children()
-      .first()
-      .click();
+      cy.get(".rt-tbody").children().first().click();
 
       // assert that the current location is the result entry page
-      cy.location()
-        .then((location) => {
-          const curentLocation = location.href.split('/').reverse()[0];
+      cy.location().then((location) => {
+        const curentLocation = location.href.split("/").reverse()[0];
 
-          expect(curentLocation).to.eq('LabResultEntry');
-        });
+        expect(curentLocation).to.eq("LabResultEntry");
+      });
     });
-    it('should navigate to the lab result entry page when a reported order is clicked', () => {
+
+    it("should navigate to the lab result entry page when a reported order is clicked", () => {
       cy.navigateToLabWorkflow();
 
-      cy.get('#test-status-dropdown')
-      .select('Reported');
+      cy.get("#test-status-dropdown").select("Reported");
 
-      cy.get('.rt-tbody')
-      .children()
-      .first()
-      .click();
+      cy.get(".rt-tbody").children().first().click();
 
       // assert that the current location is the result entry page
-      cy.location()
-        .then((location) => {
-          const curentLocation = location.href.split('/').reverse()[0];
+      cy.location().then((location) => {
+        const curentLocation = location.href.split("/").reverse()[0];
 
-          expect(curentLocation).to.eq('LabResultEntry');
-        });
+        expect(curentLocation).to.eq("LabResultEntry");
+      });
     });
-    it('should navigate to the lab result entry page when a taken order is clicked', () => {
+
+    it("should navigate to the lab result entry page when a taken order is clicked", () => {
       cy.navigateToLabWorkflow();
 
-      cy.get('#test-status-dropdown')
-      .select('Taken');
+      cy.get("#test-status-dropdown").select("Taken");
 
-      cy.get('.rt-tbody')
-      .children()
-      .first()
-      .click();
+      cy.get(".rt-tbody").children().first().click();
 
       // assert that the current location is the result entry page
-      cy.location()
-        .then((location) => {
-          const curentLocation = location.href.split('/').reverse()[0];
+      cy.location().then((location) => {
+        const curentLocation = location.href.split("/").reverse()[0];
 
-          expect(curentLocation).to.eq('LabResultEntry');
-        });
+        expect(curentLocation).to.eq("LabResultEntry");
+      });
     });
   });
-  describe('Result Entry', () => {
+
+  describe("Result Entry", () => {
     beforeEach(() => {
       cy.login();
       cy.navigateToHomePage();
-      cy.server()
-        .fetchOrders()
-        .fetchEncounters();
+      cy.server().fetchOrders().fetchEncounters();
 
       cy.fetchConcepts();
     });
 
     afterEach(() => {
-     cy.navigateToHomePage();
+      cy.navigateToHomePage();
     });
-    it('should enter results for an ordered order', () => {
-      cy.navigateToLabWorkflow();
-      cy.get('#test-status-dropdown')
-      .select('Ordered');
 
-      cy.get('.rt-tbody')
-      .children()
-      .first()
-      .click();
+    it("should enter results for an ordered order", () => {
+      cy.navigateToLabWorkflow();
+      cy.get("#test-status-dropdown").select("Ordered");
+
+      cy.get(".rt-tbody").children().first().click();
 
       // assert that the current location is the result entry page
-      cy.location()
-        .then((location) => {
-          const curentLocation = location.href.split('/').reverse()[0];
+      cy.location().then((location) => {
+        const curentLocation = location.href.split("/").reverse()[0];
 
-          expect(curentLocation).to.eq('LabResultEntry');
-        });
+        expect(curentLocation).to.eq("LabResultEntry");
+      });
 
-    // Enter form values
+      // Enter form values
 
-    // Enter value for Specimen Collection Date
-    // TODO: would have liked to selectt he current day but querying that has been troublesome
-    cy.get('.specimen-collection-date')
-    .find('.react-datepicker__input-container')
-    .find('svg')
-    .click({ force: true});
+      // Enter value for Specimen Collection Date
+      // TODO: would have liked to selectt he current day but querying that has been troublesome
+      cy.get(".specimen-collection-date")
+        .find(".react-datepicker__input-container")
+        .find("svg")
+        .click({ force: true });
 
-    cy.get('.react-datepicker-popper')
-    .find('.react-datepicker')
-    .find('.react-datepicker__month-container')
-    .find('.react-datepicker__month')
-    .find('.react-datepicker__week')
-    .first()
-    .find('.react-datepicker__day')
-    .first()
-    .click({ force: true});
+      cy.get(".react-datepicker-popper")
+        .find(".react-datepicker")
+        .find(".react-datepicker__month-container")
+        .find(".react-datepicker__month")
+        .find(".react-datepicker__week")
+        .first()
+        .find(".react-datepicker__day")
+        .first()
+        .click({ force: true });
 
-    // click estimated checkbox
-    cy.get('.estimated-checkbox')
-      .find('.checkbox')
-      .click({ force: true });
+      // click estimated checkbox
+      cy.get(".estimated-checkbox").find(".checkbox").click({ force: true });
 
+      // Select test location
+      cy.get(".test-location > div > span > select").select("Other", {
+        force: true,
+      });
 
-    // Select test location
-    cy.get('.test-location > div > span > select')
-      .select('Other', { force: true});
+      // Select result date field
+      cy.get(".obs-date-field")
+        .find(".react-datepicker__input-container")
+        .find("svg")
+        .click({ force: true });
 
-    // Select result date field
-    cy.get('.obs-date-field')
-    .find('.react-datepicker__input-container')
-    .find('svg')
-    .click({ force: true});
+      cy.get(".react-datepicker-popper")
+        .find(".react-datepicker")
+        .find(".react-datepicker__month-container")
+        .find(".react-datepicker__month")
+        .find(".react-datepicker__week")
+        .first()
+        .find(".react-datepicker__day")
+        .first()
+        .click({ force: true });
 
-    cy.get('.react-datepicker-popper')
-    .find('.react-datepicker')
-    .find('.react-datepicker__month-container')
-    .find('.react-datepicker__month')
-    .find('.react-datepicker__week')
-    .first()
-    .find('.react-datepicker__day')
-    .first()
-    .click({ force: true});
+      cy.get(".form-group")
+        .find(".obs-component")
+        .find("div")
+        .find("div")
+        .first()
+        .find("input")
+        .type("7");
 
-    cy.get('.form-group')
-    .find('.obs-component')
-    .find('div')
-    .find('div')
-    .first()
-    .find('input')
-    .type('7')
+      // Click Save form
+      cy.get(".btn.btn-success").click({ force: true });
 
-    // Click Save form
-    cy.get('.btn.btn-success')
-      .click({ force: true})
-    
-    // Assert that loader is visible
-    cy.get('.custom-loader')
-    .first()
-    .should('be.visible'); 
+      // Assert that loader is visible
+      cy.get(".custom-loader").first().should("be.visible");
     });
-  })
+  });
 });
