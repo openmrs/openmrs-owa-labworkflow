7c532c83710fc508977a80add9a5ca0497035b37
Brandon Istenes <bistenes@gmail.com>
2020-06-02 17:47:12 -0700

    Use API login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,56 +26,43 @@
 
 import { MOCKS } from "./constants";
 
-Cypress.Commands.add('login', (username = Cypress.env('username'), password = Cypress.env('password')) => {
-  cy.visit('/openmrs');
+Cypress.Commands.add(
+  "login",
+  (
+    username = Cypress.env("username"),
+    password = Cypress.env("password"),
+  ) => {
+    const token = window.btoa(`${username}:${password}`);
+    cy.request({
+      method: "GET",
+      url: "/openmrs/ws/rest/v1/session",
+      headers: {
+        Authorization: `Basic ${token}`
+      },
+    });
+  }
+);
 
-  // const getUserName = $el => $el.find('input#username');
-
-  cy.get('body')
-    .find('input#username')
-    .clear()
-    .type(username)
-    .should('have.value', username);
-
-  // const getUserPassword = $el => $el.find('input#password');
-
-  cy.get('body')
-    .find('input#password')
-    .clear()
-    .type(password)
-    .should('have.value', password);
-
-  // get the first available location
-  cy.get('#sessionLocation').get('li').first().click();
-
-
-  cy.get('body')
-    .find('input#login-button')
-    .click();
-});
-
-Cypress.Commands.add('logout', () => {
+Cypress.Commands.add("logout", () => {
   // ennsure user is logged in before using this command
-  cy.get('li.logout')
-    .find('a')
-    .click();
+  cy.get("li.logout").find("a").click();
 });
 
-Cypress.Commands.add('fetchOrders', () => {
+Cypress.Commands.add("fetchOrders", () => {
   cy.server();
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.ORDERS.URL,
     response: MOCKS.ORDERS.RESPONSE,
     status: 200,
   });
 });
 
-Cypress.Commands.add('fetchEncounters', () => {
+Cypress.Commands.add("fetchEncounters", () => {
   cy.server();
   MOCKS.ENCOUNTERS.forEach((mock) => {
     cy.route({
-      method: 'GET',
+      method: "GET",
       url: mock.URL,
       response: mock.RESPONSE,
       status: 200,
@@ -83,11 +70,11 @@ Cypress.Commands.add('fetchEncounters', () => {
   });
 });
 
-Cypress.Commands.add('fetchConcepts', () => {
+Cypress.Commands.add("fetchConcepts", () => {
   cy.server();
   MOCKS.CONCEPTS.forEach((mock) => {
     cy.route({
-      method: 'GET',
+      method: "GET",
       url: mock.URL,
       response: mock.RESPONSE,
       status: 200,
@@ -95,23 +82,23 @@ Cypress.Commands.add('fetchConcepts', () => {
   });
 });
 
-Cypress.Commands.add('fetchSystemSettiings', () => {
+Cypress.Commands.add("fetchSystemSettiings", () => {
   cy.server();
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.dateAndTimeFormat.URL,
     response: MOCKS.SYSTEM_SETTIINGS.dateAndTimeFormat.RESPONSE,
     status: 200,
   });
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.labResultsEntryEncounterType.URL,
     response: MOCKS.SYSTEM_SETTIINGS.labResultsEntryEncounterType.RESPONSE,
     status: 200,
   });
 
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.didNotPerformQuestion.URL,
     response: MOCKS.SYSTEM_SETTIINGS.didNotPerformQuestion.RESPONSE,
     status: 200,
@@ -125,56 +112,56 @@ Cypress.Commands.add('fetchSystemSettiings', () => {
   // });
 
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.didNotPerformAnswer.URL,
     response: MOCKS.SYSTEM_SETTIINGS.didNotPerformAnswer.RESPONSE,
     status: 200,
   });
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.testOrderNumberConcept.URL,
     response: MOCKS.SYSTEM_SETTIINGS.testOrderNumberConcept.RESPONSE,
     status: 200,
   });
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.locationOfLaboratory.URL,
     response: MOCKS.SYSTEM_SETTIINGS.locationOfLaboratory.RESPONSE,
     status: 200,
   });
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.estimatedCollectionDateQuestion.URL,
     response: MOCKS.SYSTEM_SETTIINGS.estimatedCollectionDateQuestion.RESPONSE,
     status: 200,
   });
 
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.estimatedCollectionDateAnswer.URL,
     response: MOCKS.SYSTEM_SETTIINGS.estimatedCollectionDateAnswer.RESPONSE,
     status: 200,
   });
 
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.didNotPerformReason.URL,
     response: MOCKS.SYSTEM_SETTIINGS.didNotPerformReason.RESPONSE,
     status: 200,
   });
 
   cy.route({
-    method: 'GET',
+    method: "GET",
     url: MOCKS.SYSTEM_SETTIINGS.labResultsDateConcept.URL,
     response: MOCKS.SYSTEM_SETTIINGS.labResultsDateConcept.RESPONSE,
     status: 200,
   });
 });
 
-Cypress.Commands.add('navigateToLabWorkflow', () => {
-  cy.visit('/openmrs/owa/labworkflow/index.html#/');
+Cypress.Commands.add("navigateToLabWorkflow", () => {
+  cy.visit("/openmrs/owa/labworkflow/index.html#/");
 });
 
-Cypress.Commands.add('navigateToHomePage', () => {
-  cy.visit('/openmrs/index.htm?lang=en_us');
+Cypress.Commands.add("navigateToHomePage", () => {
+  cy.visit("/openmrs/index.htm?lang=en_us");
 });
