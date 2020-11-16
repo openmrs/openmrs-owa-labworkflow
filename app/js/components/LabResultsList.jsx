4257b3140d3b946105514bc186ddd8e70850c4bb
Mark Goodrich <mark.goodrich@live.com>
2020-11-16 09:19:09 -0500

    fix Lab Results List so it displays nested obs (#188)

--- app/js/components/LabResultsList.jsx
+++ app/js/components/LabResultsList.jsx
@@ -327,7 +327,12 @@ export class LabResultsList extends PureComponent {
     const getPatientLabResults = () => {
       // build a list of all obs from the encounters that are of type LabSet or Test
       return encounters.reduce((acc, encounter) => {
-        const obs = encounter.obs ? encounter.obs.filter(o => isLabSet(o) || isTest(o)) : [];
+        let obs = encounter.obs ? encounter.obs : [];
+        // flatten obs groups into a single list of obs
+        while (obs.some(o => o.groupMembers)) {
+          obs = obs.flatMap(o => (o.groupMembers ? o.groupMembers : o));
+        }
+        obs = obs.filter(o => isLabSet(o) || isTest(o));
         return [...acc, ...obs];
       }, {});
     };
